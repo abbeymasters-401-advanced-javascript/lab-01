58606b1c9ddd82031867e76f56c0220b4f95af0b
Abbey Masters <abbeykmasters@gmail.com>
2019-09-16 17:18:15 -0700

    fixing travis

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-// eslint-disable-next-line no-unused-vars
 const validator = require('./lib/validator.js');
 
-// console.log(validator.isString('hello world'));
+console.log(validator.isString('hello world'));
