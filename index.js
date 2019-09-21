8b0baa1f23dfc42e3db7d6e34037dcc4c77a467e
Abbey Masters <abbeykmasters@gmail.com>
2019-09-20 17:40:38 -0700

    got function written in model.js

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-
 const validator = require('./lib/validator.js');
-
 console.log(validator.isString('hello world'));
+
+const DocumentCollection = require('./lib/document-collection');
+
+const documents = new DocumentCollection('./documents');
+
+// write some code to exercise your document collection
+
+const exampleObject = {
+  name: 'abbey'
+};
+
+documents.save(exampleObject)
+  .then(() => {
+    console.log(exampleObject);
+    documents.get(exampleObject.id)
+      .then(returnedObject => {
+        console.log(returnedObject);
+      })
+      .then(() => {
+        documents.getAll()
+          .then(res => {
+            console.log(res);
+          });
+      });
+  });
+
