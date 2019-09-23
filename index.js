a4d736ef62ea31aad688278c113476fdaf8f6ed5
Abbey Masters <abbeykmasters@gmail.com>
2019-09-23 14:22:52 -0700

    finished lab

--- index.js
+++ index.js
@@ -2,11 +2,53 @@ const validator = require('./lib/validator.js');
 console.log(validator.isString('hello world'));
 
 const DocumentCollection = require('./lib/document-collection');
-
 const documents = new DocumentCollection('./documents');
+const Database = require('./lib/database');
+
+
+const personOne = { 
+  firstName: 'abbey',
+  lastName: 'masters',
+  married: 'false',
+  kids: 0
+};
+
+const personTwo = {
+  firstName: 'allison',
+  lastName: 'busse',
+  married: 'true',
+  kids: 5
+};
+
+Database.connect('db');
+
+const model = require('./models/example-model');
+
+model.create(personOne)
+  .then((res) => {
+    console.log(res);
+    model.findById(res.id)
+      .then(res => {
+        console.log(res);
+      });
+  });
+
+model.create(personTwo)
+  .then((res) => {
+    console.log(res);
+    model.findById(res.id)
+      .then(res => {
+        console.log(res);
+      });
+  });
+
+model.find()
+  .then((result) => {
+    console.log('all:', result);
+  });
+
 
 // write some code to exercise your document collection
-
 const exampleObject = {
   name: 'abbey'
 };
@@ -25,4 +67,3 @@ documents.save(exampleObject)
           });
       });
   });
-
